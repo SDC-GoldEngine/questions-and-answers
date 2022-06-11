a25cc56a685be82f29d9e826a47ef5ebcdf8fbf2
Shannon Largman <shannon.largman@gmail.com>
2022-06-11 14:36:24 -0700

    add insert queries for questions/answers

--- questions/questions.dal.js
+++ questions/questions.dal.js
@@ -44,7 +44,8 @@ module.queryQuestionsByProductId = async (productId, page, count) => {
 };
 
 module.queryAnswersByQuestionId = async (questionId, page, count) => {
-  const answers = await db.query(`
+  const answers = await db.query(
+    `
     SELECT * FROM (
       SELECT
         answer_id,
@@ -62,9 +63,62 @@ module.queryAnswersByQuestionId = async (questionId, page, count) => {
     INNER JOIN answers_photos ap
       ON ap.answer_id = a.answer_id
     ORDER BY date DESC, id ASC;
-  `, [questionId, count, (page - 1) * count]);
+  `,
+    [questionId, count, (page - 1) * count],
+  );
 
   return answers.rows;
 };
 
-module.
+module.insertQuestion = async (productId, body, name, email) => {
+  const result = await db.query(
+    `
+    INSERT INTO question (
+      product_id,
+      question_body,
+      asker_name,
+      asker_email
+    ) VALUES ($1, $2, $3, $4);
+  `,
+    [productId, body, name, email],
+  );
+
+  // TODO: what to return?
+  return result;
+};
+
+const photoValues = (photos) => {
+  if (photos.length === 0) {
+    return '';
+  }
+
+  return `INSERT INTO answers_photos (
+    answer_id,
+    url
+  ) VALUES ${photos.map((photo) => `(inserted_id, '${photo}')`)};
+  `;
+};
+
+module.insertAnswer = async (questionId, body, name, email, photos) => {
+  const result = await db.query(
+    `
+    INSERT INTO answers (
+      question_id,
+      body,
+      answerer_name,
+      answerer_email
+    ) VALUES ($1, $2, $3, $4)
+    RETURNING answer_id INTO inserted_id;
+
+    ${photoValues(photos)}
+    `,
+    [questionId, body, name, email],
+  );
+
+  // TODO: what to return?
+  return result;
+};
+
+module.incrementHelpfulness = async (table, id) => {};
+
+module.report = async (table, id) => {};
