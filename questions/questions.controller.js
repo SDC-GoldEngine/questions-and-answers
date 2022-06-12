03c460df244edf4bea219b54cb7881648532f856
Shannon Largman <shannon.largman@gmail.com>
2022-06-12 01:58:31 -0700

    complete getQuestions controller

--- questions/questions.controller.js
+++ questions/questions.controller.js
@@ -1,47 +1,48 @@
 const dal = require('./questions.dal.js');
 
 const formatQuestions = (rows) => {
-    const questions = [];
-    const i = -1;
+  const questions = [];
+  let i = -1;
 
-    for (const row of rows) {
-      if (questions[i]?.question_id !== row.question_id) {
-        i++;
-        questions[i] = {
-          question_id: row.question_id,
-          question_body: row.question_body,
-          question_date: row.question_date,
-          asker_name: row.asker_name,
-          question_helpfulness: row.question_helpfulness,
-          reported: false,
-          answers: {},
+  for (const row of rows) {
+    if (questions[i]?.question_id !== row.question_id) {
+      i += 1;
+      questions[i] = {
+        question_id: row.question_id,
+        question_body: row.question_body,
+        question_date: row.question_date,
+        asker_name: row.asker_name,
+        question_helpfulness: row.question_helpfulness,
+        reported: false,
+        answers: {},
+      };
+    }
+
+    if (row.answer_id) {
+      if (!questions[i].answers[row.answer_id]) {
+        questions[i].answers[row.answer_id] = {
+          id: row.answer_id,
+          body: row.body,
+          date: row.date,
+          answerer_name: row.answerer_name,
+          helpfulness: row.helpfulness,
+          photos: [],
         };
       }
 
-      if (row.answer_id) {
-        if (!questions[i].answers[row.answer_id]) {
-          questions[i].answers[row.answer_id] = {
-            id: row.answer_id,
-            body: row.body,
-            date: row.date,
-            answerer_name: row.answerer_name,
-            helpfulness: row.helpfulness,
-            photos: [],
-          };
-        }
-
-        if (row.id) {
-          questions[i].answers[row.answer_id].photos.push({
-            id: row.id,
-            url: row.url,
-          });
-        }
+      if (row.id) {
+        questions[i].answers[row.answer_id].photos.push({
+          id: row.id,
+          url: row.url,
+        });
       }
     }
   }
+  return questions;
 };
+
 module.exports.getQuestions = async (context) => {
-  const result = await dal.queryQuestionsByProductId(
+  const rows = await dal.queryQuestionsByProductId(
     context.productId,
     context.count,
     context.page,
@@ -49,7 +50,7 @@ module.exports.getQuestions = async (context) => {
 
   return {
     product_id: context.productId,
-    results: formatQuestions(result.rows),
+    results: formatQuestions(rows),
   };
 };
 
