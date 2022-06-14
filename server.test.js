1ef00ed24ea2a4ed53e8167061f3e907806daa26
Shannon Largman <shannon.largman@gmail.com>
2022-06-13 20:37:09 -0700

    add test skeleton

---
+++ server.test.js
@@ -0,0 +1,19 @@
+
+describe('Questions and Answers API', () => {
+  describe('GET /qa/questions', () => {
+  };
+  describe('GET /qa/questions/:question_id/answers', () => {
+  };
+  describe('POST /qa/questions', () => {
+  };
+  describe('POST /qa/questions/:question_id/answers', () => {
+  };
+  describe('POST /qa/questions/:question_id/helpful', () => {
+  };
+  describe('POST /qa/questions/:question_id/report', () => {
+  };
+  describe('POST /qa/answers/:answer_id/helpful', () => {
+  };
+  describe('POST /qa/answers/:answer_id/report', () => {
+  };
+};
